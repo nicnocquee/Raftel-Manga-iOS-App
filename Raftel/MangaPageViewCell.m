8f04724c5662319d79fa4038430e480b6237150b
Nico Prananta <nicnocquee@yahoo.com>
2014-12-07 22:03:09 +0900

    double tap to zoom

--- Raftel/MangaPageViewCell.m
+++ Raftel/MangaPageViewCell.m
@@ -13,10 +13,20 @@
 - (void)awakeFromNib {
     // Initialization code
     [self.scrollView setDelegate:self];
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didDoubleTapScrollView:)];
+    [tapGesture setNumberOfTapsRequired:2];
+    [self.scrollView addGestureRecognizer:tapGesture];
 }
 
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
     return self.imageView;
+}
+
+- (void)didDoubleTapScrollView:(UITapGestureRecognizer *)gesture {
+    CGPoint touch = [gesture locationInView:self.imageView];
+    if (self.scrollView.zoomScale == 1) [self.scrollView zoomToRect:(CGRect){touch, CGSizeMake(20, 20)} animated:YES];
+    else [self.scrollView setZoomScale:1 animated:YES];
 }
 
 @end
